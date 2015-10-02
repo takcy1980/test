d1dd5aabd95eb1ed1c326baf5cb68222cfb7a486
robert <rversteegt@live.nl>
2015-10-02 22:34:10 +0200

    Blank form for creating new shops. No back-end functionality exists yet.

--- src/main/java/com/pse/fotoz/controllers/producer/dashboard/ProducerShopsController.java
+++ src/main/java/com/pse/fotoz/controllers/producer/dashboard/ProducerShopsController.java
@@ -4,6 +4,7 @@ import com.pse.fotoz.dbal.HibernateEntityHelper;
 import com.pse.fotoz.dbal.entities.Shop;
 import com.pse.fotoz.helpers.mav.ModelAndViewBuilder;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,29 @@ public class ProducerShopsController {
             public String redirect = request.getRequestURL().toString();
         });
         mav.setViewName("producer/dashboard/shops.twig");
+
+        return mav;
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/new")
+    public ModelAndView provideNewShopForm(HttpServletRequest request) {
+        ModelAndView mav = ModelAndViewBuilder.empty().
+                    withProperties(request).
+                    build();
+
+        List<String> existingShopLogins = 
+                HibernateEntityHelper.all(Shop.class).stream().
+                map(s -> s.getLogin()).
+                collect(Collectors.toList());
+        
+        mav.addObject("existing_shop_logins", existingShopLogins);
+        mav.addObject("page", new Object() {
+            public String lang = request.getSession().
+                    getAttribute("lang").toString();
+            public String uri = "/producer/dashboard/shops";
+            public String redirect = request.getRequestURL().toString();
+        });
+        mav.setViewName("producer/dashboard/shops_new.twig");
 
         return mav;
     }
