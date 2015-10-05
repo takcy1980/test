37bddfee0456550113cf76821f36aa6c23263b65
Tarkan Akcay <310054544@QQ0005CG5071MDX.code1.emi.philips.com>
2015-10-05 20:32:22 +0200

    login via spring security

--- src/main/java/com/pse/fotoz/helpers/Authentication/LoginProviderCustomers.java
+++ src/main/java/com/pse/fotoz/helpers/Authentication/LoginProviderCustomers.java
@@ -2,7 +2,14 @@
  */
 package com.pse.fotoz.helpers.Authentication;
 
+import com.pse.fotoz.dbal.HibernateEntityHelper;
+import com.pse.fotoz.dbal.entities.Customer_accounts;
+import com.pse.fotoz.dbal.entities.ProducerAccounts;
+import com.pse.fotoz.helpers.encryption.PasswordHash;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,19 +22,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoginProviderCustomers implements AuthenticationProvider {
  
+    
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
-        if (name.equals("klant") && password.equals("klant")) {
-            List<GrantedAuthority> grantedAuths = new ArrayList();
-            grantedAuths.add(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
-            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
-            return auth;
+        Authentication auth = null;
+
+        Iterator<Customer_accounts> accounts = HibernateEntityHelper.all(Customer_accounts.class).
+                stream().
+                filter(a -> a.getLogin().equals(name)).iterator();
+        
+        while(accounts.hasNext()){
+            Customer_accounts account = accounts.next();
+            Boolean check = false;
+            try{
+                check = PasswordHash.validatePassword(password, account.getPasswordHash());
+            } catch(NoSuchAlgorithmException | InvalidKeySpecException e){
+                //@TODO
+                //Hoe vanuit hier een exception opvangen / communiceren naar gebruiker?
+            }
+            
+            if(check){
+                List<GrantedAuthority> grantedAuths = new ArrayList();
+                grantedAuths.add(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+                auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+            }
         }
-        else {
-            return null;
-        }
+
+        return auth;
+
     }
  
     @Override
