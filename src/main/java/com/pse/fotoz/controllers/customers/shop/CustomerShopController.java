08584b596a94c86051397d3e14e5c92baae48fdc
Tarkan Akcay <310054544@QQ0005CG5071MDX.code1.emi.philips.com>
2015-10-11 20:16:09 +0200

    combined controller for customer shop. Login/logout.

--- src/main/java/com/pse/fotoz/controllers/customers/shop/CustomerShopController.java
+++ src/main/java/com/pse/fotoz/controllers/customers/shop/CustomerShopController.java
@@ -1,9 +1,15 @@
 package com.pse.fotoz.controllers.customers.shop;
 
+import com.pse.fotoz.dbal.HibernateEntityHelper;
+import com.pse.fotoz.dbal.entities.Shop;
 import com.pse.fotoz.helpers.mav.ModelAndViewBuilder;
 import com.pse.fotoz.properties.LocaleUtil;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,42 +17,54 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("customers/shop")
 public class CustomerShopController {
- 
+    
+    
+
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView loadLoginScreen(HttpServletRequest request) {
         ModelAndView mav = ModelAndViewBuilder.empty().
-                    withProperties(request).
-                    build();
+                withProperties(request).
+                build();
+        
+     
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName(); //get logged in username
+
+        mav.addObject("username", name);
 
         mav.addObject("page", new Object() {
             public String lang = request.getSession().
                     getAttribute("lang").toString();
             public String uri = "/customers/shop";
             public String redirect = request.getRequestURL().toString();
-        });            
-
-        mav.setViewName("customers/shop/index.twig");
-
-        return mav;
-    }
-
-    /*
-    @Issue 
-    not yet implemented
-     */
-    @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView serviceLoginRequest() {
-        ModelAndView mav = new ModelAndView();
-
-        mav.setViewName("customers/shop/index.twig");            
-
-        mav.addObject("labels", LocaleUtil.getProperties("en"));
-        mav.addObject("page", new Object() {
-            public String lang = "en";
         });
-        mav.addObject("error", 
-                "The login functionality is not yet implemented.");
+
+        mav.setViewName("customers/home/index.twig");
 
         return mav;
     }
+    
+    
+        @RequestMapping(method = RequestMethod.GET, value = "/fotos")
+    public ModelAndView displayShops(HttpServletRequest request) {
+        ModelAndView mav = ModelAndViewBuilder.empty().
+                    withProperties(request).
+                    build();
+
+        List<Shop> shops = HibernateEntityHelper.all(Shop.class);
+        
+        mav.addObject("shops", shops);
+        mav.addObject("page", new Object() {
+            public String lang = request.getSession().
+                    getAttribute("lang").toString();
+            public String uri = "/customers/shop/fotos";
+            public String redirect = request.getRequestURL().toString();
+        });
+        mav.setViewName("customers/shop/fotos.twig");
+
+        return mav;
+    }
+    
+
 }
