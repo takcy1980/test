ed6156ca9b7ecd05b912841ce9e9eeedf5f255c2
robert <rversteegt@live.nl>
2015-10-23 18:51:53 +0200

    Spring MVC unit testing setup.

---
+++ src/test/java/com/pse/fotoz/integration/controllers/TestMvcConfig.java
@@ -0,0 +1,56 @@
+package com.pse.fotoz.integration.controllers;
+
+import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.loader.Loader;
+import com.mitchellbosecke.pebble.loader.ServletLoader;
+import com.mitchellbosecke.pebble.spring.PebbleViewResolver;
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ *
+ * @author Robert
+ */
+@Configuration
+@ComponentScan(basePackages = {
+    "com.pse.fotoz.controllers.producer",
+    "com.pse.fotoz.controllers.customers",
+    "com.pse.fotoz.controllers.photographers",
+    "com.pse.fotoz.controllers.common"})
+@EnableWebMvc
+public class TestMvcConfig {
+    
+    @Autowired
+    private ServletContext servletContext;
+    
+    @Bean
+    public ViewResolver viewResolver() {
+        PebbleViewResolver viewResolver = new PebbleViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix("");
+        viewResolver.setPebbleEngine(pebbleEngine());
+        return viewResolver;
+    }
+    
+    @Bean
+    public PebbleEngine pebbleEngine() {
+        PebbleEngine engine = new PebbleEngine(templateLoader());
+
+        /* @Issue
+         Disables template caching, should be removed in a production environment
+         */
+        engine.setTemplateCache(null);
+        
+        return engine;
+    }
+    
+    @Bean
+    public Loader templateLoader() {
+        return new ServletLoader(servletContext);
+    }
+}
