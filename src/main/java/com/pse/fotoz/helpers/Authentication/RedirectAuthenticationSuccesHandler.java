480bf7b88d006e9f71a56cf850d744a0c773c78b
gijshendrikx <gijshendrikx1990@gmail.com>
2015-11-27 12:51:48 +0100

    Fixed redirect after succesfull login

--- src/main/java/com/pse/fotoz/helpers/Authentication/RedirectAuthenticationSuccesHandler.java
+++ src/main/java/com/pse/fotoz/helpers/Authentication/RedirectAuthenticationSuccesHandler.java
@@ -7,14 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
  *
  * @author Gijs
  */
-public class RedirectAuthenticationSuccesHandler implements 
-        AuthenticationSuccessHandler {
+public class RedirectAuthenticationSuccesHandler  extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     
@@ -25,17 +24,23 @@ public class RedirectAuthenticationSuccesHandler implements
         if (request.getParameter("redirect") != null) {
             redirectStrategy.sendRedirect(request, response, 
                 request.getParameter("redirect"));
-        } else {
-            redirectStrategy.sendRedirect(request, response, "/app");
+        }
+        else {
+           
+            super.onAuthenticationSuccess(request, response, authentication);
+
         }
     }
     
+    @Override
     public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
         this.redirectStrategy = redirectStrategy;
     }
     
+    @Override
     protected RedirectStrategy getRedirectStrategy() {
         return redirectStrategy;
     }
 
 }
+
